82e400904cbace197709dad904ad269a223d0142
TFGcybx <1042298441@qq.com>
2024-11-17 00:07:20 +0800

    调整引入element plus

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { Button } from 'vant'
+import { Button, Sidebar, SidebarItem } from 'vant'
 import router from './router'
 import store from './store'
 import 'vant/lib/index.css';
@@ -11,5 +11,7 @@ const app = createApp(App)
 app.use(router)
 app.use(store)
 app.use(Button)
+app.use(Sidebar)
+app.use(SidebarItem)
 
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,17 @@
 <!-- Home.vue -->
  <template>
-    <div>Home</div>
-    <van-button type="primary" size="largr">大按钮</van-button>
-    <button @click="min">-</button>
-    <span>购物车</span>
-    <button @click="add">+</button>
+    <div>
+        ing
+    </div>
  </template>
 
- <script>
- import { useStore } from 'vuex'
+<script>
+import { useStore } from 'vuex'
+
 export default {
     name: 'Home',
-    setup() {
-        const store = useStore()
-        const add = () => {
-            store.dispatch('add', 1)
-        }
-        const min = () => {
-            store.dispatch('min', 1)
-        }
-        return { add, min }
+    components: {
+        SideBar
     }
 }
 </script>
